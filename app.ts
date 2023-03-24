81a35ebd990cd2ebf8bc031f464efaa75e0d336a
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2023-03-24 13:38:14 -0400

    union types

--- app.ts
+++ app.ts
@@ -1,42 +1,64 @@
 const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string] //tuple type - fixed length array
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple type - fixed length array
 } = {
-    // const person = {
-    name: 'Sandy',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-}
+  // const person = {
+  name: "Sandy",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
 
-console.log(person.name)
+console.log(person.name);
 
 for (const hobby of person.hobbies) {
-    //anything that can be done with a string, can be done with hobby
-    console.log(hobby.toUpperCase())
+  //anything that can be done with a string, can be done with hobby
+  console.log(hobby.toUpperCase());
 }
 
 // person.role.push('admin'); //***TS doesn't catch this error with tuple
 // person.role[1] = 10; can't be done with tuple
-person.role = [0, 'user'] //this works
+person.role = [0, "user"]; //this works
 
-console.log(person.role)
+console.log(person.role);
 
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
 
-enum Role { ADMIN, READ_ONLY, AUTHOR};
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
 
 const personTwo = {
-    name: 'Tim',
-    age: 30,
-    hobbies: ['Running', 'Cooking'],
-    role: Role.ADMIN,
-}
+  name: "Tim",
+  age: 30,
+  hobbies: ["Running", "Cooking"],
+  role: Role.ADMIN,
+};
 
 if (personTwo.role === Role.ADMIN) {
-    console.log('admin')
+  console.log("admin");
 }
+
+//union type - more flexible, multiple types of values
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  //otherwise typescript won't accept +
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine("John", "Jane");
+console.log(combinedNames)
