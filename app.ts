b7cfc7f5a0d4dda653763ed72d6293e86730411a
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2023-04-21 16:05:37 -0400

    function types and callbacks

--- app.ts
+++ app.ts
@@ -9,6 +9,14 @@ function printResult(num: number) {
 
 console.log(printResult(add(5, 12))); //undefined
 
+//function types and callbacks
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
+}
+
+printResult(add(5, 12));
+
 //function types
 let combineValues: (a: number, b: number) => number;
 
@@ -17,3 +25,7 @@ combineValues = add;
 // combineValues = 5;
 
 console.log(combineValues(8, 8))
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+});
